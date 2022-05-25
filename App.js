3b7844f6f1239d82853e8d2ad6ab224359290cdd
eltoncfc <elton.martins@live.com>
2022-05-25 19:37:59 -0300

    installing lib native-stack

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {StatusBar, SafeAreaView} from 'react-native';
 import AppRotas from './src/rotas/AppRotas';
+import Produtor from './src/telas/produtor';
 
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar />
       <AppRotas />
+      {/* <Produtor /> */}
     </SafeAreaView>
   );
 }
